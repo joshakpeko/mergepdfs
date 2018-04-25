4a80dc880f55cd8e2fc0deea9705303bf9f01473
Josh Akpeko <joshuaakpeko31@gmail.com>
2018-04-25 11:13:47 +0000

    First Commit

---
+++ README.txt
@@ -0,0 +1,15 @@
+mergepdfs shows a basic use of pypdf2 to merge pdf files
+
+*How it works:
+
+**On a terminal prompt, type this:
+
+>>> ./mergepdfs.py file1.pdf file2.pdf fileN.pdf
+(replace file1, file2, ..., fileN by actual files names)
+
+**To make the script executable you can run this prior to execution
+
+>>> chmod u+x mergepdfs.py
+
+**After execution, you'll obtail a file named "output.pdf"
+that actually contains each page of provided pdf files.
---
+++ mergepdfs.py
@@ -0,0 +1,31 @@
+# mergepdfs leverages PyPDF2 package to concatenate various pdf files
+# into one. Files names may be provided on command line as arguments
+import os, sys
+
+import PyPDF2
+
+
+def main():
+    files = sys.argv[1:]
+    if len(files) < 2:
+        print("Provide at least 2 pdf files to merge")
+        os.Exit(1)
+    mergeFiles(*files)
+    return
+
+def mergeFiles(*files):
+    pdfMerger = PyPDF2.PdfFileMerger()
+    for f in files:
+        try:
+            pdfMerger.append(f)
+        except Exception as ex:
+            print(ex.with_traceback(None))
+            continue
+
+    pdfMerger.write("output.pdf")
+    pdfMerger.close()
+    return
+
+
+if __name__ == "__main__":
+    main()
